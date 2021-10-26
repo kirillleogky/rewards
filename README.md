6d4b1568c43941821f605244b74d5354d16eada6
Kiryl Lehki <kirillmargheriti@gmail.com>
2021-10-26 14:01:24 +0300

    Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# rewards
 Peer rewards application
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './style/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
